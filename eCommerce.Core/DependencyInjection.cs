988167e5c14bb4d9fc2095400c4f371234664313
KunigiriMohan <kmohanr5@gmail.com>
2026-05-16 20:28:47 +0530

    Add project files.

---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace eCommerce.Core;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+
+        // Add other infrastructure services here
+        return services;
+    }
+}
+
