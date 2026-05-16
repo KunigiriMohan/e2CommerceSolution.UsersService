a4168fe84b7e4ddcaf5c9044e8510435d728a004
KunigiriMohan <kmohanr5@gmail.com>
2026-05-16 22:00:06 +0530

    Intermediate_Changes

---
+++ eCommerce.Core/DTO/AuthenticationResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Core.DTO;
+
+public record AuthenticationResponse(Guid UserID, string? Email, string? PersonName, string? Gender,
+        string? Token, bool success);
+
---
+++ eCommerce.Core/DTO/RegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Core.DTO;
+
+public record RegisterRequest(string? Email, string? Password, string? PersonName, GenderOptions Gender);
+
